ffe3574e3aecb22319370ca7b95e45d5c0ebaa59
Oluwanifemi <latunde.nifemi2016@gmail.com>
2023-04-25 16:06:33 +0100

    Added animation to progress bar

--- src/pages/components/SplashLoading.jsx
+++ src/pages/components/SplashLoading.jsx
@@ -1,12 +1,25 @@
+import { useState, useEffect } from "react";
+import { ProgressBar } from "react-bootstrap";
 import "./SplashLoading.css";
 
 const SplashLoading = () => {
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
   return (
     <div className="wrapper">
-      <img src="/public/assets/splashScreen/FirstImage.png" alt="" />
-      <p className="text-white text-center">
-        Welcome Dreamers, <br /> building your experience
-      </p>
+      <div>
+        <img src="/public/assets/splashScreen/FirstImage.png" alt="" />
+        <p className="text-white text-center fw-bold">
+          Welcome Dreamers, <br /> building your experience
+        </p>
+      </div>
+      <div className="progress-wrapper">
+        <ProgressBar now={completed} />
+      </div>
     </div>
   );
 };
