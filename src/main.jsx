f408d3de84a59707154bf4c84b8e68a73aa7d960
Oluwanifemi <latunde.nifemi2016@gmail.com>
2023-05-05 13:18:17 +0100

    Toast Container

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -23,6 +24,7 @@ import UpcomingEvents from "./pages/UpcomingEvents";
 
 import { IconContext } from "react-icons";
 import { LocationProvider } from "./pages/LocationContext.jsx";
+import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
@@ -88,6 +90,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <React.StrictMode>
         <IconContext.Provider value={{ size: 24 }}>
           <RouterProvider router={router} />
+          <ToastContainer />
         </IconContext.Provider>
       </React.StrictMode>
     </PersistGate>
