6fdc2f692c168c4224022d2f5a8e8d58d49f393d
Oluwanifemi <latunde.nifemi2016@gmail.com>
2023-05-05 13:15:59 +0100

    Added Signin and SignUp functionality

--- src/pages/Accounts/SignIn.jsx
+++ src/pages/Accounts/SignIn.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import API_URL from "../../config";
 import "./SignUp.css";
 
 const SignIn = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Create an object with the user's login credentials
+    const credentials = {
+      email: email,
+      password: password,
+    };
+
+    // Make an API call to the backend for user authentication
+    axios
+      .post(`${API_URL}/api/v1/auth/login`, credentials)
+      .then((response) => {
+        // Authentication successful
+        toast.success("Authentication successful");
+        // Store the authorization token in local storage or state for future API calls
+        localStorage.setItem("authToken", response.data.payload.token);
+        navigate("/discover");
+      })
+      .catch((error) => {
+        // Authentication failed
+        setError("Email address or password is invalid");
+        toast.error("Email address or password is invalid");
+      });
+  };
   return (
     <div className="container">
       <div className="img-container">
@@ -15,27 +56,33 @@ const SignIn = () => {
         />
       </div>
       <h2 className="fw-bold text-left heading">Sign In</h2>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" className="input-form" required />
+          <Form.Control
+            type="email"
+            className="input-form"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" className="input-form" required />
+          <Form.Control
+            type="password"
+            className="input-form"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
         </Form.Group>
 
-        <Button
-          variant="primary"
-          type="submit"
-          className="signup-btn"
-          onClick={() => navigate("/discover")}
-        >
+        <Button variant="primary" type="submit" className="signup-btn">
           Continue
         </Button>
       </Form>
-      <p className="text-center text-white mt-4">Forgot Password</p>
     </div>
   );
 };
--- src/pages/Accounts/SignUp.jsx
+++ src/pages/Accounts/SignUp.jsx
@@ -1,48 +1,90 @@
-// import { useState } from "react";
+import { useState } from "react";
+// import { useSelector } from "react-redux";
 import { Button, Form } from "react-bootstrap";
 import "./SignUp.css";
 import { useNavigate } from "react-router-dom";
-import { ThreeDots } from "react-loader-spinner";
-// import { toast } from "react-toastify";
+import axios from "axios";
+// import { ThreeDots } from "react-loader-spinner";
+
+import { toast } from "react-toastify";
+import API_URL from "../../config";
 // import { useSelector, useDispatch } from "react-redux";
 
 const SignUp = () => {
   const navigate = useNavigate();
-  // const [formData, setFormData] = useState({
-  //   name: "",
-  //   email: "",
-  //   password: "",
-  // });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  // // load states from redux
-  // const dispatch = useDispatch();
-  // const loading = useSelector((state) => state.account.loading);
-  // //   const isLoggedIn = useSelector((state) => state.account.isLoggedIn);
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    console.log(name.split(" ")[0]);
+  };
 
-  // // destructure register data from state
-  // const { password, email, name } = formData;
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-  // // create function to handle input onChange
-  // const onChange = (event) => {
-  //   setFormData((prevState) => ({
-  //     ...prevState,
-  //     [event.target.name]: event.target.value,
-  //   }));
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Create an object with the user's signup data
+    const userData = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    // Make an API call to the backend for user signup
+    axios
+      .post(`${API_URL}/api/v1/auth/signup`, userData)
+      .then((response) => {
+        // Signup successful
+
+        toast.success("Signup successful");
+        navigate("/choose-artist");
+      })
+      .catch((error) => {
+        // Signup failed
+        if (error.response && error.response.status === 409) {
+          setError("Email address is already registered");
+        } else {
+          setError("Signup failed");
+        }
+      });
+  };
+
+  // const handleVerifyCode = () => {
+  //   const verificationData = {
+  //     email: email,
+  //     code: verificationCode,
+  //   };
+
+  //   // Make an API call to the backend to verify the code
+  //   axios
+  //     .post(
+  //       "https://festa-api.herokuapp.com/api/v1/auth/verify",
+  //       verificationData
+  //     )
+  //     .then((response) => {
+  //       // Verification successful
+  //       const { authToken } = response.data;
+  //       console.log(authToken);
+  //       toast.success("Verification successful");
+  //       // Save the authToken in local storage or state for later use
+  //       // Redirect the user to the login page or perform any other action
+  //       navigate("/login");
+  //     })
+  //     .catch((error) => {
+  //       // Verification failed
+  //       setError("Verification failed");
+  //     });
   // };
-
-  // const onSubmit = (event) => {
-  //   event.preventDefault();
-
-  //   //
-  //   if (!password || !email || !name) {
-  //     toast.error("Please complete all fields");
-  //     return;
-  //   }
-
-  //   // dispatch(registerAccount(formData));
-  // };
-
-  // redirect to dashboard page if user is logged in
 
   return (
     <div className="container">
@@ -55,16 +97,16 @@ const SignUp = () => {
       </div>
       <h1 className="fw-bold text-left heading">Sign Up</h1>
 
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Form.Group className="mb-3" controlId="formBasicText">
           <Form.Label>Name</Form.Label>
           <Form.Control
             type="text"
-            // value={name}
+            value={name}
             name="name"
             required
             className="input-form"
-            // onChange={onChange}
+            onChange={handleNameChange}
           />
         </Form.Group>
 
@@ -75,8 +117,8 @@ const SignUp = () => {
             name="email"
             required
             className="input-form"
-            // value={email}
-            // onChange={onChange}
+            value={email}
+            onChange={handleEmailChange}
           />
         </Form.Group>
 
@@ -84,27 +126,44 @@ const SignUp = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            // value={password}
+            value={password}
             name="password"
-            minLength={8}
+            minLength={6}
             className="input-form"
             required
-            // onChange={onChange}
+            onChange={handlePasswordChange}
           />
         </Form.Group>
 
-        <Button
-          className="signup-btn"
-          variant="primary"
-          type="submit"
-          onClick={() => navigate("/choose-artist")}
-        >
+        <Button className="signup-btn" variant="primary" type="submit">
           Continue
           {/* <span className="tracking-[0.02em] text-white text-center font-bold text-3xl flex justify-center">
             {loading ? <ThreeDots color="#fff" height={27} /> : "Continue"}
           </span> */}
         </Button>
       </Form>
+
+      {/* {verificationCode && (
+        <div>
+          <Form.Group className="mb-3" controlId="formBasicCode">
+            <Form.Label>Verification Code</Form.Label>
+            <Form.Control
+              type="text"
+              value={verificationCode}
+              onChange={(e) => setVerificationCode(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Button
+            className="verify-btn"
+            variant="primary"
+            onClick={handleVerifyCode}
+          >
+            Verify
+          </Button>
+        </div>
+      )} */}
+
       <p
         onClick={() => navigate("/login")}
         className="text-center text-white mt-4"
