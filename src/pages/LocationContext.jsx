497c96317dfb0b4ae048d50e1fad2bc5c6d1984b
Oluwanifemi <latunde.nifemi2016@gmail.com>
2023-05-05 13:15:10 +0100

    Persisted state for location until signout

--- src/pages/LocationContext.jsx
+++ src/pages/LocationContext.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export const LocationContext = React.createContext();
+export const LocationContext = createContext();
 
 export const LocationProvider = ({ children }) => {
-  const [location, setLocation] = useState({});
+  const [location, setLocation] = useState({
+    city: "",
+    state: "",
+    zipcode: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const savedLocation = JSON.parse(localStorage.getItem("location"));
+    if (savedLocation) {
+      setLocation(savedLocation);
+    }
+  }, []);
 
   const updateLocation = (city, state, zipcode) => {
-    setLocation((prevLocation) => ({
-      ...prevLocation,
-      city,
-      state,
-      zipcode,
-    }));
+    const newLocation = { city, state, zipcode };
+    setLocation(newLocation);
+    localStorage.setItem("location", JSON.stringify(newLocation));
   };
 
   return (
