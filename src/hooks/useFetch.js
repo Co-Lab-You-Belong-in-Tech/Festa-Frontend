f033a5d0d7414ff68057ad4a8dba50ca200a0409
Oluwanifemi <latunde.nifemi2016@gmail.com>
2023-05-08 03:18:55 +0100

    Implemented event details

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import API_URL from "../config";
+// which component is g-iv-i-ng you problem?,home
+export default function useFetch(url, dataKey = "payload") {
+  const { token } = useSelector((state) => state.account);
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/v1${url}`, {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.statusText == "OK") {
+          setData(response.data[dataKey]);
+        } else throw response;
+      } catch (error) {
+        setError(error.response?.data.message ?? error.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+    return () => {
+      setData(undefined);
+      setLoading(true);
+      setError(null);
+    };
+  }, [url]);
+
+  return [data, loading, error];
+}
